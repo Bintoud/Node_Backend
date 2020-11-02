f79e055ac4ec9e111a25aab49fe09a2b425f00e3
Bintoud <68588582+Bintoud@users.noreply.github.com>
2020-11-02 17:00:13 +0100

    finish day one

---
+++ express_static/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'))
+
+
+const port = 3001
+app.listen(port, () => {
+    console.log('Server started on port: ' + port);
+  });
